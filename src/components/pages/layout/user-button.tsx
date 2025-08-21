bd03a9b76d1af23ecbc296fecfd515e49f50a9aa
amann-codes <aman.prajapati3000@gmail.com>
2025-08-21 22:46:21 +0530

    added user button to log out and show name and email of looged in user

---
+++ src/components/pages/layout/user-button.tsx
@@ -0,0 +1,39 @@
+import { Avatar } from "@/components/ui/avatar";
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from "@/components/ui/dropdown-menu";
+import { Separator } from "@/components/ui/separator";
+import { signIn } from "@/lib/auth/auth";
+import { LogOut, User } from "lucide-react";
+import { signOut, useSession } from "next-auth/react";
+
+export function UserButton() {
+    const handleSignOut = async () => {
+        try {
+            await signOut({ callbackUrl: "/login" });
+        } catch (error) {
+            console.error("Sign out failed:", error);
+        }
+    };
+    const { data: session } = useSession();
+    return <div>
+        <DropdownMenu>
+            <DropdownMenuTrigger className="rounded-full border p-3 bg-gray-300">
+                <User />
+            </DropdownMenuTrigger>
+            <DropdownMenuContent className="px-3">
+                <div>
+                    <p>
+                        {session?.user?.name}
+                    </p>
+                    <p>
+                        {session?.user?.email}
+                    </p>
+                </div>
+                <Separator />
+                <DropdownMenuItem onClick={handleSignOut} >
+                    Sign out
+                    <LogOut />
+                </DropdownMenuItem>
+            </DropdownMenuContent>
+        </DropdownMenu>
+    </div>
+}
